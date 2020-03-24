15d645a0fe8abffc6afaef230efc979bbe698d75
Omikunle Okiki Joshua <okikiomikunle@gmail.com>
2020-03-24 18:08:43 +0100

    js task

---
+++ first_task.js
@@ -0,0 +1,8 @@
+var name = "okiki"
+console.log(name);
+var courses = ["Html", "Css", "Javascript", "python"]
+console.log(courses);
+var i; for(i=0;i<=200;i++) { if(i % 2 == 0)
+    console.log(i);
+}
+
